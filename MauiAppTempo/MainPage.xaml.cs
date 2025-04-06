995f21f9c46657ebcdbe1dc02f1a6a57b42f4bf8
migue <migue@DESKTOP-POH8G6H>
2025-04-05 23:08:46 -0300

    vdrv

--- MauiAppTempo/MainPage.xaml.cs
+++ MauiAppTempo/MainPage.xaml.cs
@@ -1,25 +1,57 @@
-﻿namespace MauiAppTempo
+﻿using MauiAppTempo.Models;
+using MauiAppTempo.Services;
+using System.Net.Http;
+
+namespace MauiAppTempo
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if (t != null)
+                    {
+                        string dados_previsao = $"Latitude: {t.lat} \n" +
+                                                $"Longitude: {t.lon} \n" +
+                                                $"Nascer do Sol: {t.sunrise} \n" +
+                                                $"Pôr do Sol: {t.sunset} \n" +
+                                                $"Temp Máx: {t.temp_max} °C\n" +
+                                                $"Temp Min: {t.temp_min} °C\n" +
+                                                $"Clima: {t.description} \n" +
+                                                $"Velocidade do Vento: {t.speed} m/s\n" +
+                                                $"Visibilidade: {t.visibility} metros";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else
+                    {
+                        await DisplayAlert("Cidade não encontrada", "Não foi possível encontrar a sua cidade. Verifique novamente.", "OK");
+                        lbl_res.Text = string.Empty;
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a sua cidade.";
+                }
+            }
+            catch (HttpRequestException)
+            {
+                await DisplayAlert("Sem conexão", "Verifique sua conexão com a internet e verifique novamente.", "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", $"Ocorreu um erro: {ex.Message}", "OK");
+            }
         }
     }
-
 }
